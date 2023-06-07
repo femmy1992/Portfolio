2d9c0da250581185e572922872dc1afadda3717a
Femi Ojo <femmy1992@yahoo.com>
2023-06-06 22:46:00 -0400

    update

---
+++ terraform/variable.tf
@@ -0,0 +1,15 @@
+variable "path" {
+default = ["index.html", "error.html"]
+}
+variable "key" {
+  default = ["index.html", "error.html"]
+}
+variable "path2" {
+  default = "lamda_function.zip"
+}
+variable "myregion" {
+  default = "region"
+}
+variable "accountId" {
+  default = "accountid"
+}
